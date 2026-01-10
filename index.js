51919d2d35dd4b49438aa9d3590cc4bb21c46411
jeddelich@gmail.com <jeddelich@gmail.com>
2026-01-10 14:35:55 -0500

    no-button-refresh

--- index.js
+++ index.js
@@ -4,7 +4,17 @@
 async function renderMovies(id) {
     const moviesPromise = await fetch('http://www.omdbapi.com/?i=' + id + '&apikey=806b3177');
     const moviesData = await moviesPromise.json()
-    console.log(moviesData);
 };
 
 renderMovies('tt1285016');
+
+// search bar (variables)
+
+const search = document.getElementById("search-bar")
+const form = document.getElementById("search-bar__form")
+
+// no refresh on the submit button 
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault()
+})

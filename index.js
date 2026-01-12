e5c89a3091376a1f1e05d32d0beae7659fae7495
jeddelich@gmail.com <jeddelich@gmail.com>
2026-01-12 17:02:24 -0500

    runtimes-added

--- index.js
+++ index.js
@@ -4,63 +4,58 @@ const searchForm = document.getElementById("search__form");
 const spinner = document.querySelector(".fa-spinner");
 const container = document.querySelector(".header__container");
 const movieList = document.querySelector(".movie__list");
-
+let movieArray = [];
 
 // main function that is run
 
-searchForm.addEventListener('submit', function(event) {
+searchForm.addEventListener("submit", function (event) {
+  // my important const and let variables
+  const searchBar = document.getElementById("search-bar");
+  let search = searchBar.value;
 
-    // my important const and let variables
-    const searchBar = document.getElementById("search-bar");
-    let search = searchBar.value;
+  // no website refresh on submit
+  event.preventDefault();
 
-    // no website refresh on submit 
-    event.preventDefault();
-
-        renderMovies(search)
+  renderMovies(search);
 });
 
 /* 
 
 functions used within main 
 
-*/ 
+*/
 
 async function renderMovies(search) {
-    // loading state initiated
-    spinner.classList += " loading"  
-    container.classList += " move-forward"
-    movieList.style.opacity = 0;
-    
-    const moviesPromise = await fetch(`http://www.omdbapi.com/?s=${search}&apikey=806b3177`);
-    const moviesData = await moviesPromise.json()
-    const firstSix = moviesData.Search.splice(0, 6)  
-    console.log(firstSix)
+  // loading state initiated
+  spinner.classList += " loading";
+  container.classList += " move-forward";
+  movieList.style.opacity = 0;
 
-    setTimeout(() => {
-    moviesHTML(firstSix);
-    }, 1000)
-};
+  const moviesPromise = await fetch(
+    `http://www.omdbapi.com/?s=${search}&apikey=806b3177`
+  );
+  const moviesData = await moviesPromise.json();
+  const firstSix = moviesData.Search.splice(0, 6);
 
-function moviesHTML(firstSix) {
-   
-    spinner.classList.remove ("loading") 
-    container.classList.remove ("move-forward") 
-    movieList.style.opacity = 1;  
+  movieArray = [];
 
-    movieList.innerHTML = null
-    if (firstSix[5].Poster.status === 404) {
-        console.log("testing")
-    }
-    else {
-        console.log("failed")
-    }
-        
-    for (i = 0; i < firstSix.length; i++) {
+  calculateRuntimes(firstSix);
 
-        if (firstSix[i].Poster === "N/A") {
-            movieList.innerHTML += 
-            `<div class="movie">
+  setTimeout(() => {
+    moviesHTML(firstSix, movieArray);
+  }, 1000);
+}
+
+function moviesHTML(firstSix, movieArray) {
+  spinner.classList.remove("loading");
+  container.classList.remove("move-forward");
+  movieList.style.opacity = 1;
+
+  movieList.innerHTML = null;
+
+  for (let i = 0; i < firstSix.length; i++) {
+    if (firstSix[i].Poster === "N/A") {
+      movieList.innerHTML += `<div class="movie">
                 <div class="movie__poster--wrapper">
                     <div class="movie__poster--unavailable">No Picture<br>In Database</div>
                 </div>
@@ -68,14 +63,12 @@ function moviesHTML(firstSix) {
                     <div class="movie__title">${firstSix[i].Title}</div>
                     <div class="movie__details">
                         <div class="movie__year">${firstSix[i].Year}</div>
-                        <div class="movie__rated">PG-13</div>
+                        <div class="movie__rated">${movieArray[i]}</div>
                     </div>
                 </div>
-            </div>`    
-        }
-        else {
-            movieList.innerHTML += 
-            `<div class="movie">
+            </div>`;
+    } else {
+      movieList.innerHTML += `<div class="movie">
                 <div class="movie__poster--wrapper">
                     <img src="${firstSix[i].Poster}" class="movie__poster">
                 </div>
@@ -83,12 +76,42 @@ function moviesHTML(firstSix) {
                     <div class="movie__title">${firstSix[i].Title}</div>
                     <div class="movie__details">
                         <div class="movie__year">${firstSix[i].Year}</div>
-                        <div class="movie__rated">PG-13</div>
+                        <div class="movie__rated">${movieArray[i]}</div>
                     </div>
                 </div>
-            </div>`
-        }
+            </div>`;
     }
+  }
+
+  container.style.height = "fit-content";
+}
+
+// fetches the length of the movies using the i= given by the fetched s= 
+
+async function calculateRuntimes(firstSix) {
+  for (let i = 0; i < firstSix.length; i++) {
+    const movieId = firstSix[i].imdbID;
+    const moviesPromise2 = await fetch(
+      `http://www.omdbapi.com/?i=${movieId}&apikey=806b3177`
+    );
+    const movieData2 = await moviesPromise2.json();
+    const movieDataNumbers = movieData2.Runtime.replace(" min", "");
     
-    container.style.height = "fit-content";
+    // formats to hours and minutes
+    
+    if (Number(movieDataNumbers) > 119) {
+      const hours = Math.floor(movieDataNumbers / 60);
+      const minutes = movieDataNumbers % 60;
+      const time = hours + " hrs " + minutes + " mins";
+      movieArray.push(time);
+    } else if (Number(movieDataNumbers) > 59) {
+      const hours = Math.floor(movieDataNumbers / 60);
+      const minutes = movieDataNumbers % 60;
+      const time = hours + " hr " + minutes + " mins";
+      movieArray.push(time);
+    } else {
+      const time2 = movieDataNumbers + " mins";
+      movieArray.push(time2);
+    }
+  }
 }
